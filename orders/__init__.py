66599405d107c324a933f3e6dd1bfae029318cd0
jenny-smart <jenny@hers.com.tw>
2026-06-24 08:22:54 +0800

    Add quick order subprogram package

---
+++ orders/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""快速訂單子程式套件。"""
+
+__all__ = [
+    "old_customer_quick_order",
+    "new_customer_parser",
+    "line_notice_generator",
+    "order_converter",
+    "stored_value_makeup",
+]
